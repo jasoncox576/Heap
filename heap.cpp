d1e7bb5f7c00bdcb90e77d10f8bafe498a2b010a
Jason Cox <jasongenecox@gmail.com>
2017-10-19 19:41:50 -0500

    Added a method to print tree in level order

--- heap.cpp
+++ heap.cpp
@@ -37,6 +37,43 @@ public:
 };
 
 
+void print_level_order(HeapTree* node) {
+	if(!node) return;
+	queue<HeapTree*> nodesQueue;
+
+	int current_level = 1;
+	int next_level = 0;
+
+	nodesQueue.push(node);
+
+	while(!nodesQueue.empty()) {
+		HeapTree *currnode = nodesQueue.front();
+		nodesQueue.pop();
+
+		current_level--;
+		if(currnode) {
+			cout << currnode->data << " ";
+		}
+		if(current_level == 0) cout << endl;
+
+		if(currnode->left) {
+
+			nodesQueue.push(currnode->left);
+			next_level++;
+		}
+
+		if(currnode->right) {
+
+			nodesQueue.push(currnode->right);
+			next_level++;
+		}
+
+		if(current_level == 0) swap(current_level, next_level);
+	} 
+
+}
+
+
 
 
 
