98eb948de93c2144d2d1aa455c8666ffb877ad29
Jason Cox <jasongenecox@gmail.com>
2017-10-22 20:14:32 -0500

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -1,8 +1,8 @@
-
 #include <cstdlib>
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -19,16 +19,14 @@ public:
         data = val;
         
     }
+    HeapTree() {
+
+        data = 0;
+    }
     int data;
     HeapTree* left;
     HeapTree* right;
-    
-    int construct(vector<int> arr, int index) {
-        this->data = arr[index];
-        this->left->data = construct(arr, index*2 + 1);
-        this->right->data = construct(arr, index*2 + 2);
-        return arr[index];
-    }
+
     
     void print() {
         
@@ -73,6 +71,27 @@ void print_level_order(HeapTree* node) {
 
 }
 
+    
+HeapTree construct(vector<int>& arr, HeapTree node, int index) {
+        cout << "in construct method" << endl;
+
+        if(index < arr.size()) {
+            cout << "passed if conditional " << arr[index] << endl;
+            HeapTree temp = HeapTree(arr[index]);
+            node = temp;
+            cout << "Node being set to: " << arr[index] << " at index " << index << endl;
+            //for(int x : arr) cout << x << endl;
+            HeapTree left = construct(arr, *node.left, index*2 + 1);
+            node.left = &left;
+
+            HeapTree right = construct(arr, *node.right, index*2 + 2);
+            node.right = &right;
+        }
+        //node.left = &left;
+        //node.right = &right;
+        return node;
+    }
+
 
 
 
@@ -97,18 +116,28 @@ int main(int argc, char** argv) {
 
     vector<int> myvec;
     
-    myvec = getInts();
-    
+    //myvec = getInts();
+    myvec = {67, 23, 40, 85, 1, 2, 30, 90, 123};
+
     std::sort(myvec.begin(), myvec.end());
+    std::reverse(myvec.begin(), myvec.end());
+    for(int x : myvec) cout << x << " ";
+        cout << endl;
     
 
 
     
-    HeapTree root(myvec[0]);
-    root
+    HeapTree root(10);
+    cout << "Root node constructed" << endl;
+
+    root = construct(myvec, root, 0);
+    cout << "Heap fully constructed" << endl;
+
+
+    print_level_order(&root);
     
-    
+
+
     return 0;
 }
-
 
