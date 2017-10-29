9e424e78839e0679916ec24fce8ac6a764666c89
Jason Cox <jasongenecox@gmail.com>
2017-10-29 16:05:56 -0500

    Added more Heap functionality

--- heap.cpp
+++ heap.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <algorithm>
 #include <queue>
+#include <deque>
+#include <stack>
 
 using namespace std;
 
@@ -30,11 +32,104 @@ public:
     HeapTree* right;
 
 
-    void print() {
 
-    }
 
 };
+
+
+HeapTree* searchN(HeapTree* root, int start, int end) {
+	if(start == end) return root;
+	if(start != end) {
+		if(root->left) {
+			HeapTree* leftreturn = searchN(root->left, start*2+1, end);
+			if(leftreturn->data) {
+				root = leftreturn;
+				return root;
+			}
+		}
+
+		if(root->right) {
+			HeapTree* rightreturn = searchN(root->right, start*2+2, end);
+			if(rightreturn->data) {
+				root = rightreturn;
+				return root;
+			}
+		}
+	}
+
+
+		return new HeapTree;
+
+
+}
+
+
+
+
+void max_heapify(HeapTree* root) {
+
+	if(root->left) {
+
+		if(root->left->data > root->data) {
+			int leftdata = root->left->data;
+			int rootdata = root->data;
+			root->left->data = rootdata;
+			root->data = leftdata;
+		}
+		max_heapify(root->left);
+	}
+
+	if(root->right) {
+
+		if(root->right->data > root->data) {
+			int rightdata = root->right->data;
+			int rootdata = root->data;
+			root->right->data = rootdata;
+			root->data = rightdata;
+
+		}
+		max_heapify(root->right);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void deleteMax(HeapTree* root, int heapSize) {
+
+	HeapTree* replacerNode = searchN(root, 0, heapSize);
+	int replacerData = replacerNode->data;
+	root->data = replacerData;
+
+	HeapTree* replacerptr;
+	*replacerNode = *replacerptr;
+	replacerNode->left = NULL;
+	replacerNode->right = NULL;
+
+	max_heapify(root);
+
+
+}
+
+
+
+
+
+
 
 
 
@@ -53,8 +148,8 @@ void print_level_order(HeapTree* node) {
 		nodesQueue.pop();
 
 		current_level--;
-		if(currnode) {
-			cout << currnode->data << " ";
+		if(currnode->data) {
+				cout << currnode->data << " ";
 		}
 		if(current_level == 0) cout << endl;
 
@@ -76,25 +171,21 @@ void print_level_order(HeapTree* node) {
 }
 
 
-HeapTree* construct(vector<int>& arr, HeapTree* node, int index) {
-        cout << "in construct method" << endl;
+HeapTree* construct(vector<int>& arr, HeapTree* node, int index, deque<HeapTree*>& dbucket) {
 
         if(index < arr.size()) {
-            cout << "passed if conditional " << arr[index] << endl;
+
             HeapTree* temp = new HeapTree(arr[index]);
             node = temp;
-            node->left = new HeapTree();
-            node->left = construct(arr, node->left, index*2 + 1);
-            if(node->left) cout << "printing left: " << node->left->data << endl;
+            dbucket.push_front(node);
+            node->left = construct(arr, node->left, index*2 + 1, dbucket);
 
-            node->right = new HeapTree();
-            node->right = construct(arr, node->right, index*2 + 2);
+            node->right = construct(arr, node->right, index*2 + 2, dbucket);
 
         }
-        //node.left = &left;
-        //node.right = &right;
         return node;
     }
+
 
 
 
@@ -116,34 +207,65 @@ vector<int> getInts() {
 
 }
 
+void deleteHeap(deque<HeapTree*> tree) {
+
+    // deleting all of the nodes in tree (getting rid of the heap)
+    for(HeapTree* ptr : tree) {
+    	delete ptr;
+    }
+
+}
+
+
+
+
+
+
+
 int main(int argc, char** argv) {
 
     vector<int> myvec;
+    int heap_size = -1;
 
-    //myvec = getInts();
-   //vec = {67, 23, 40, 85, 1, 2, 30, 90, 123};
-   //yvec = {100, 43, 45, 23, 54, 2, 1, 3, 5, 80, 98, 45};
-    for(int i = 1;i < 100; ++i) {
-    	myvec.push_back(i);
-    }
+   //myvec = {100, 43, 45, 23, 54, 2, 1, 3, 5, 80, 98, 45};
 
-    std::sort(myvec.begin(), myvec.end());
-    std::reverse(myvec.begin(), myvec.end());
+    myvec = {12, 4243, 34, 12, 60, 43, 5, 34, 6, 3, 5, 7, 8, 23, 21, 23, 4, 44, 55, 66, 45, 20, 23, 45, 69, 39, 46, 13};
+    heap_size = myvec.size() - 1;
+
+    //std::sort(myvec.begin(), myvec.end());
+    //std::reverse(myvec.begin(), myvec.end());
     for(int x : myvec) cout << x << " ";
         cout << endl;
 
 
 
     HeapTree root(10);
-    cout << root.data << endl;
-    cout << "Root node constructed" << endl;
 
 
-    root = *construct(myvec, &root, 0);
+    deque<HeapTree*> dbucket;
+
+    root = *construct(myvec, &root, 0, dbucket);
     cout << "Heap fully constructed" << endl;
+
+    max_heapify(&root);
 
 
     print_level_order(&root);
+
+    deleteMax(&root, heap_size);
+    cout << endl;
+    cout << endl;
+
+    print_level_order(&root);
+    cout << "root node deleted" << endl;
+
+
+    cout << "deleted" << endl;
+    cout << "It worked!" << endl;
+
+
+    deleteHeap(dbucket);
+
 
 
 
